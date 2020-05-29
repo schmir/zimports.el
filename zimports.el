5b2f477cb2ecc4d31abfdef124f4be124e361638
Ralf Schmitt <ralf@systemexit.de>
2020-05-30 01:53:49 +0200

    Make this compatible with melpa

--- zimports.el
+++ zimports.el
@@ -1,5 +1,30 @@
+;;; zimports.el --- Reformat python imports with zimports
+
+;; URL: https://github.com/schmir/zimports.el
+;; Version: 0
+;; Package-Requires: ((emacs "26.1"))
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3, or (at your
+;; option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This can be used to run zimports on python code
+
 (require 'cl-lib)
 (require 'projectile)
+
+;;; Code:
 
 (defgroup zimports nil
   "Reformat Python code with \"zimports\"."
